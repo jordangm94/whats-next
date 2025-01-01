51fe7b5446459fd8d061600f411bc8410a1ee854
jordangm94 <jordan.guerrero.martinez@gmail.com>
2025-01-01 13:18:53 -0500

    Adjust global css + add top title bar that says 'tasks'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,41 @@
+import { Box, Typography } from "@mui/material";
+import "../app/globals.css";
+
+// import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+// import { LocationOn } from "@mui/icons-material";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#1976d2",
+        height: 50,
+        width: "100",
+        position: "sticky",
+      }}
+    >
+      <Typography variant="h5" sx={{ color: "white" }}>
+        Tasks
+      </Typography>
+    </Box>
+  );
+}
+
+{
+  /* <AppBar>
+  <Toolbar
+    sx={{
+      display: "flex",
+      justifyContent: "center",
+    }}
+    variant="dense"
+  >
+    <Typography variant="h5" sx={{ color: "white" }}>
+      Tasks
+    </Typography>
+  </Toolbar>
+</AppBar> */
 }
